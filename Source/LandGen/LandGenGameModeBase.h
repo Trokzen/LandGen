a718bbe37be7f2815bfddbbbf4f56a60026d686d
Robert <Nadzar@yandex.ru>
2024-04-08 15:05:27 +0300

    Create LandGenGameModeBase.h

---
+++ Source/LandGen/LandGenGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LandGenGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LANDGEN_API ALandGenGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
